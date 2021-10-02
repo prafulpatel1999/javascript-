ab3bf22496223e273b36fc3016bafc37cc310b9c
prafulpatel1999 <88098316+prafulpatel1999@users.noreply.github.com>
2021-10-02 12:18:28 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,25 @@
+console.log("hello world")
+let day=prompt("which day is this")
+
+if (day == 'monday'){
+   alert("get ready for the job")
+}
+if (day == 'tuesday'){
+    alert("what a bad day")
+ }
+ if (day == 'wednesday'){
+    alert("getting a better day ")
+ }
+ if (day == 'thursday'){
+    alert("getting a much better day")
+ }
+ if (day == 'friday'){
+    alert("weekend is coming")
+ }
+ if (day == 'saturday'){
+    alert("weekend started ")
+ }
+ if (day == 'sunday'){
+    alert("weekend is going to start")
+ }
+
